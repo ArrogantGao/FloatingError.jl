6d718c09e943daa9766b74f6cfda475c2e03f9a2
Xuanzhao Gao <gaoxuanzhao@gmail.com>
2024-04-09 15:15:29 +0800

    add poly

--- src/core.jl
+++ src/core.jl
@@ -11,3 +11,6 @@ Base.show(io::IO, x::EFloat) = print(io, x.n, " ± ", x.ϵ)
 Base.:+(x::EFloat{T}, y::EFloat{T}) where{T} = EFloat(x.n + y.n, eps(abs(x.n + y.n) + x.ϵ + y.ϵ) + x.ϵ + y.ϵ)
 Base.:-(x::EFloat{T}, y::EFloat{T}) where{T} = EFloat(x.n - y.n, eps(abs(x.n - y.n) + x.ϵ + y.ϵ) + abs(x.ϵ - y.ϵ))
 Base.:*(x::EFloat{T}, y::EFloat{T}) where{T} = EFloat(x.n * y.n, T(sqrt(5)) * eps((abs(x.n) + x.ϵ) * (abs(y.n) + y.ϵ)) + abs(x.n * y.ϵ) + abs(y.n * x.ϵ))
+
+Base.zero(::Type{EFloat{T}}) where{T} = EFloat(zero(T))
+Base.muladd(x::EFloat{T}, y::EFloat{T}, z::EFloat{T}) where{T} = x * y + z
